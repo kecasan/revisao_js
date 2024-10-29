77487ef028b580ba899ad9c94c31f9b46ca6f05f
Érica Santos <143044036+kecasan@users.noreply.github.com>
2024-10-29 20:24:59 -0300

    desc

---
+++ funcoes.js
@@ -0,0 +1,12 @@
+function ao_clicar() {
+    var texto = document.getElementById("clicou");
+    if (texto.style.display === "none") {
+        texto.style.display = "block"; 
+    } else {
+        texto.style.display = "none";
+    }
+}
+
+function brilha() {
+    
+}
